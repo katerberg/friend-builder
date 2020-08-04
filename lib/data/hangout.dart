b039b18842c9b27c90d6cab506562f5b5d1ebd8d
Mark Katerberg <katerberg@fastmail.fm>
2020-08-03 23:03:47 -0500

    Handles legacy fields

--- lib/data/hangout.dart
+++ lib/data/hangout.dart
@@ -22,6 +22,31 @@ class Hangout {
   String dateWithYear() => DateFormat.yMMMMd().format(this.when);
   String dateWithoutYear() => DateFormat.MMMMd().format(this.when);
 
+  static String _getHowMany(String howMany) {
+    switch (howMany) {
+      case 'Small Group':
+        return 'Small group';
+      case 'One on One':
+        return 'One on one';
+      default:
+        return howMany;
+    }
+  }
+
+  static String _getMedium(String medium) {
+    switch (medium) {
+      case 'Face to Face':
+        return 'Face to face';
+      case 'Mail':
+        return 'Text';
+      case 'Phone':
+      case 'Video':
+        return 'Call';
+      default:
+        return medium;
+    }
+  }
+
   factory Hangout.fromJson(Map<String, dynamic> parsedJson) {
     return new Hangout(
       id: parsedJson['id'] ?? Uuid().v4(),
@@ -29,8 +54,8 @@ class Hangout {
           .map((c) => EncodableContact.fromJson(c))
           .toList(),
       where: parsedJson['where'] ?? "",
-      howMany: parsedJson['howMany'] ?? "One on one",
-      medium: parsedJson['medium'] ?? "Face to face",
+      howMany: _getHowMany(parsedJson['howMany']) ?? "One on one",
+      medium: _getMedium(parsedJson['medium']) ?? "Face to face",
       when: DateTime.parse(parsedJson['when']),
     );
   }
