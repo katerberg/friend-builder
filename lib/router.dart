f6f547c02abeb3d0f831a6748e36250ddc6b51af
Mark Katerberg <katerberg@fastmail.fm>
2024-06-02 13:28:48 -0500

    Extracts splash screen

--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:friend_builder/splash_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:flutter/material.dart';
@@ -86,51 +87,9 @@ class _FriendRouterState extends State<FriendRouter> {
 
     Widget content;
     if (firstTime == null) {
-      var titleStyle = GoogleFonts.getFont(
-        'Londrina Sketch',
-        fontSize: 60,
-        fontWeight: FontWeight.w900,
-        backgroundColor: colorScheme.primary,
+      content = SplashScreen(
+        googleFontsPending: _googleFontsPending,
       );
-
-      content = FutureBuilder(
-          future: _googleFontsPending,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState != ConnectionState.done) {
-              return const SizedBox();
-            }
-
-            return ColoredBox(
-              color: colorScheme.primary,
-              child: SafeArea(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: Center(
-                        child: Text(
-                          'Friends',
-                          style: titleStyle,
-                        ),
-                      ),
-                    ),
-                    Image.asset(
-                      'logo/splash.png',
-                      fit: BoxFit.cover,
-                    ),
-                    Expanded(
-                      child: Center(
-                        child: Text(
-                          'Builder',
-                          style: titleStyle,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          });
     } else {
       content = Column(children: [
         Expanded(child: mainArea),
