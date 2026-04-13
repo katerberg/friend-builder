44be0fcb478a033bba6e27b69efd3e8ac4b6515e
Mark Katerberg <katerberg@fastmail.fm>
2026-04-13 14:18:28 -0500

    Fixes ci post clone script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -3,11 +3,18 @@
 # Fail this script if any subcommand fails.
 set -e
 
+# CocoaPods (Ruby) expects a UTF-8 locale; otherwise `pod install` can crash
+# with ASCII-8BIT / unicode_normalize errors in CI environments.
+export LANG="${LANG:-en_US.UTF-8}"
+export LC_ALL="${LC_ALL:-en_US.UTF-8}"
+
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
 # Install Flutter using git.
-git clone https://github.com/flutter/flutter.git -b stable $HOME/flutter
+if [ ! -d "$HOME/flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable "$HOME/flutter"
+fi
 export PATH="$PATH:$HOME/flutter/bin"
 
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
@@ -17,8 +24,13 @@ flutter precache --ios
 flutter pub get
 
 # Install CocoaPods using Homebrew.
-HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
-brew install cocoapods
+# Disable homebrew's automatic updates.
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_ENV_HINTS=1
+
+if ! command -v pod >/dev/null 2>&1; then
+  brew install cocoapods
+fi
 
 # Install CocoaPods dependencies.
 cd ios && pod install # run `pod install` in the `ios` directory.
